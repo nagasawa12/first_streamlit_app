2affcb114235f63c4abb5f29d4cc63c573206d99
Masayoshi Nagasawa <120117990+nagasawa12@users.noreply.github.com>
2022-12-09 01:13:13 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Avocado Toast')
+streamlit.text('🍇Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥭Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍌Hard-Boiled Free-Range Egg')
+streamlit.text('🥝Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
